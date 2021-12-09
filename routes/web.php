fa962510204a31fb134a3f5508885eaf4e8aebb1
Rohit <ronychauhan1@gmail.com>
2021-12-09 10:52:19 +0530

    layout setup

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,3 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-  
-// Route::get('/Account', function () {
-//     return view('account');
-// });
-Route::group(['middleware'=>['UserAuth']],function(){
-    Route::get('/Account', [App\Http\Controllers\UserController::class, 'edit'])->name('Account');
-
-   Route::post('/update', [App\Http\Controllers\UserController::class, 'update'])->name('update');
-
-});
